0cc4f6dd18ef4181de97d233e482aa818edb9820
The Bean <115789528+debean@users.noreply.github.com>
2022-12-12 23:07:39 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function play {
+    document.location.href = "https://game.imibuks.repl.co/"
+}
+
+function beta () {
+    document.location.href = "https://betabeans.imibuks.repl.co/"
+}
+
+function alpha () {
+    document.location.href = "https://dbn.imibuks.repl.co/"
+}
+
+function blog () {
+    document.location.href = "https://debeans.blogspot.com/"
+}
